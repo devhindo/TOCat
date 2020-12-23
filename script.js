a5b746ad5f7a41f8478b51258b47480eb6f6bb4f
devhindo <66886118+devhindo@users.noreply.github.com>
2020-12-23 03:47:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+ // solution structure
+ function reverse(str) {
+    let splitedArray = str.split("");
+    let reversedArray = splitedArray.reverse();
+    let reversedStr = reversedArray.join("");
+    return reversedStr;
+    }
+  
+    // example
+    reverse("CAT"); // TAC
+  
+
+
+   
+ 
